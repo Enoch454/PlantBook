45df9894f1a7aaa434efab6c09efd03997955e7a
enoch454 <aegc.45@gmail.com>
2021-04-20 13:10:45 -0500

    creacion de componente vue home

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">
+                Example Component
+            </div>
+            <div class="card-body">
+                I'm an example component.
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return{
+                lista_plantas: [],
+            }
+        },
+        methods: {
+            async listar() {
+                const res = await axios.get('/plantas');
+                this.lista_plantas = res.data;
+            },
+        },
+        created() {
+            this.listar();
+        },
+        mounted() {
+            //console.log('Component mounted.')
+            console.log(this.lista_plantas)
+        },
+    }
+</script>
