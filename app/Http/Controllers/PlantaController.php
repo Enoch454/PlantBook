fa4ae35e01fd72a33505a5409e5ed69f6eabc875
enoch454 <aegc.45@gmail.com>
2021-04-19 23:04:16 -0500

    Creacion de modelo, controlador y migracion

---
+++ app/Http/Controllers/PlantaController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Planta;
+use Illuminate\Http\Request;
+
+class PlantaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Planta  $planta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Planta $planta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Planta  $planta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Planta $planta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Planta  $planta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Planta $planta)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_04_10_035253_create_plantas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlantasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plantas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //----
+            $table->string('nombre');
+            $table->string('nCientifico');
+            $table->string('nAlterno');
+            $table->text('info');
+            $table->string('pathImagen');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plantas');
+    }
+}
