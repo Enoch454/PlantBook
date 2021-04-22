301f4f09d579586d44d534d3778bb1d39e18188e
enoch454 <aegc.45@gmail.com>
2021-04-22 12:44:00 -0500

    remplaso de componente ficha

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="col-md-10">
+    <div class="">
+
+        
+
         <div class="card">
             <div class="card-body">
-                <ficha-component
-                    v-bind:nombre="this.lista_plantas[0].nombre"
-                    v-bind:nCientifico="this.lista_plantas[0].nCientifico"
-                    v-bind:nAlterno="this.lista_plantas[0].nAlterno"
-                    v-bind:info="this.lista_plantas[0].info"
-                    v-bind:pathImagen="this.lista_plantas[0].pathImagen"
-                ></ficha-component>
+                <div v-for="planta in lista_plantas" :key="planta.id">
+                    <button class="ripple" @click="seleccionar(planta.id)">
+                        <img
+                            alt="una flor"
+                            class="img-fluid rounded"
+                            v-bind:src="get_pathImagen(planta.pathImagen)"
+                        />
+                    </button>
+                </div>
             </div>
         </div>
     </div>
@@ -19,21 +24,30 @@
         data () {
             return{
                 lista_plantas: [],
+                planta_seleccionada: -1,
             }
         },
         methods: {
             async listar() {
                 const res = await axios.get('/plantas');
                 this.lista_plantas = res.data;
-                console.log(this.lista_plantas)
             },
+            get_pathImagen(path_imagen){
+                var path = "http://localhost/storage/"+path_imagen;
+                console.log(path)
+                return path;
+            },
+            seleccionar(id){
+                this.planta_seleccionada = id;
+                console.log(this.planta_seleccionada);
+            }
         },
         created() {
             //console.log('Component mounted.')
             this.listar();
         },
         mounted() {
-            
+                console.log(this.lista_plantas)
         },
     }
 </script>
